564326019f600379b43b7fb08b1b8b6b41d332d6
yolbert28 <yolberttorrealba27@gmail.com>
2024-07-08 22:32:04 -0400

    sin codigo de la documentacion

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -22,57 +22,7 @@ class AuthController extends Controller implements HasMiddleware
         ];
     }
 
-    /**
-     *  @OA\Post(
-     *      path="/api/auth/login",
-     *      tags={"Auth"},
-     *      summary="Login",
-     *      description="Login",
-     *      security={{"bearer": {} }},
-     *      @OA\RequestBody(
-     *          required=true,
-     *          @OA\JsonContent(
-     *              required={"email", "password"},
-     *              @OA\Property(property="email", type="string", example="user@exam.com"),
-     *              @OA\Property(property="password", type="string", example="password")
-     *          )
-     *      ),
-     *       @OA\Response(
-     *           response=200,
-     *           description="Ok",
-     *           @OA\JsonContent(
-     *               type="object",
-     *               @OA\Property(
-     *                   property="access_token",
-     *                   type="string",
-     *                   description="access token"
-     *               ),
-     *               @OA\Property(
-     *                   property="token_type",
-     *                   type="string",
-     *                   description="token type"
-     *               ),
-     *               @OA\Property(
-     *                   property="expires_in",
-     *                   type="string",
-     *                   description="expiration time"
-     *               )
-     *           )
-     *       ),
-     *       @OA\Response(
-     *           response=422,
-     *           description="Unprocessable Content",
-     *           @OA\JsonContent(
-     *               type="object",
-     *               @OA\Property(
-     *                   property="message",
-     *                   type="string",
-     *                   description="message"
-     *               )
-     *           )
-     *       )
-     *  )
-     */
+    
     public function login(Request $request)
     {
         $credentials = $request->validate(
@@ -89,59 +39,7 @@ class AuthController extends Controller implements HasMiddleware
         return $this->respondWithToken($token);
     }
 
-    /**
-     *  @OA\Post(
-     *      path="/api/auth/register",
-     *      tags={"Auth"},
-     *      summary="Register",
-     *      description="Register",
-     *      security={{"bearer": {} }},
-     *      @OA\RequestBody(
-     *          required=true,
-     *          @OA\JsonContent(
-     *              required={"name","email", "password", "password_confirmation"},
-     *              @OA\Property(property="name", type="string", example="user"),
-     *              @OA\Property(property="email", type="string", example="user@exam.com"),
-     *              @OA\Property(property="password", type="string", example="password"),
-     *              @OA\Property(property="password_confirmation", type="string", example="password")
-     *          )
-     *      ),
-     *       @OA\Response(
-     *           response=200,
-     *           description="Ok",
-     *           @OA\JsonContent(
-     *               type="object",
-     *               @OA\Property(
-     *                   property="access_token",
-     *                   type="string",
-     *                   description="access token"
-     *               ),
-     *               @OA\Property(
-     *                   property="token_type",
-     *                   type="string",
-     *                   description="token type"
-     *               ),
-     *               @OA\Property(
-     *                   property="expires_in",
-     *                   type="string",
-     *                   description="expiration time"
-     *               )
-     *           )
-     *       ),
-     *       @OA\Response(
-     *           response=422,
-     *           description="Unprocessable Content",
-     *           @OA\JsonContent(
-     *               type="object",
-     *               @OA\Property(
-     *                   property="message",
-     *                   type="string",
-     *                   description="message"
-     *               )
-     *           )
-     *       )
-     *  )
-     */
+    
     public function register(RegisterUserRequest $request){
         $validatedData = $request->validated();
 
@@ -152,101 +50,13 @@ class AuthController extends Controller implements HasMiddleware
         return $this->respondWithToken($token);
     }
 
-    /**
-     *  @OA\Get(
-     *      path="/api/auth/me",
-     *      tags={"Auth"},
-     *      summary="User information",
-     *      description="User information",
-     *      security={{"bearer": {} }},
-     *      @OA\Response(
-     *          response=200,
-     *          description="Ok",
-     *          @OA\JsonContent(
-     *              type="object",
-     *              @OA\Property(
-     *                      property="id",
-     *                      type="integer",
-     *                      description="Id of the user"
-     *                  ),
-     *                  @OA\Property(
-     *                      property="name",
-     *                      type="string",
-     *                      description="Name of the user"
-     *                  ),
-     *                  @OA\Property(
-     *                      property="email",
-     *                      type="string",
-     *                      description="email of the user"
-     *                  ),
-     *                  @OA\Property(
-     *                      property="email_verified_at",
-     *                      type="string"
-     *                  ),
-     *                 @OA\Property(
-     *                     property="created_at",
-     *                     type="string",
-     *                     description="created date of the client"
-     *                 ),
-     *                 @OA\Property(
-     *                     property="updated_at",
-     *                     type="string",
-     *                     description="updated date of the client"
-     *                 ) 
-     *          )
-     *      ),
-     *       @OA\Response(
-     *           response=401,
-     *           description="Unauthorized",
-     *           @OA\JsonContent(
-     *               type="object",
-     *               @OA\Property(
-     *                   property="error",
-     *                   type="string",
-     *                   description="message"
-     *               )
-     *           )
-     *       )
-     *  )
-     */
+    
     public function me()
     {
         return response()->json(auth()->user(), 200);
     }
 
-    /**
-     *  @OA\Post(
-     *      path="/api/auth/logout",
-     *      tags={"Auth"},
-     *      summary="Logout",
-     *      description="Logout",
-     *      security={{"bearer": {} }},
-     *      @OA\Response(
-     *          response=200,
-     *          description="Ok",
-     *          @OA\JsonContent(
-     *               type="object",
-     *               @OA\Property(
-     *                   property="message",
-     *                   type="string",
-     *                   description="message"
-     *               )
-     *           )
-     *      ),
-     *       @OA\Response(
-     *           response=401,
-     *           description="Unauthorized",
-     *           @OA\JsonContent(
-     *               type="object",
-     *               @OA\Property(
-     *                   property="error",
-     *                   type="string",
-     *                   description="message"
-     *               )
-     *           )
-     *       )
-     *  )
-     */
+    
     public function logout()
     {
         auth()->logout();
@@ -254,49 +64,7 @@ class AuthController extends Controller implements HasMiddleware
         return response()->json(['message' => 'Successfully logged out'], 200);
     }
 
-    /**
-     *  @OA\Post(
-     *      path="/api/auth/refresh",
-     *      tags={"Auth"},
-     *      summary="Refresh",
-     *      description="Refresh",
-     *      security={{"bearer": {} }},
-     *       @OA\Response(
-     *           response=200,
-     *           description="Ok",
-     *           @OA\JsonContent(
-     *               type="object",
-     *               @OA\Property(
-     *                   property="access_token",
-     *                   type="string",
-     *                   description="access token"
-     *               ),
-     *               @OA\Property(
-     *                   property="token_type",
-     *                   type="string",
-     *                   description="token type"
-     *               ),
-     *               @OA\Property(
-     *                   property="expires_in",
-     *                   type="string",
-     *                   description="expiration time"
-     *               )
-     *           )
-     *       ),
-     *       @OA\Response(
-     *           response=401,
-     *           description="Unauthorized",
-     *           @OA\JsonContent(
-     *               type="object",
-     *               @OA\Property(
-     *                   property="error",
-     *                   type="string",
-     *                   description="message"
-     *               )
-     *           )
-     *       )
-     *  )
-     */
+    
     public function refresh()
     {
         return $this->respondWithToken(auth()->refresh());
--- app/Http/Controllers/PermissionController.php
+++ app/Http/Controllers/PermissionController.php
@@ -9,62 +9,7 @@ use Symfony\Component\HttpFoundation\Response;
 
 class PermissionController extends Controller
 {
-    /**
-     *  @OA\Get(
-     *      path="/api/permission/",
-     *      tags={"Permissions"},
-     *      summary="Get a list of permissions",
-     *      description="Get a list of permissions",
-     *      security={{"bearer": {} }},
-     *      @OA\Response(
-     *          response=200,
-     *          description="Ok",
-     *          @OA\JsonContent(
-     *              type="array",
-     *              @OA\Items(
-     *                  type="object",
-     *                  @OA\Property(
-     *                      property="id",
-     *                      type="integer",
-     *                      description="Id of the permission"
-     *                  ),
-     *                  @OA\Property(
-     *                      property="name",
-     *                      type="string",
-     *                      description="Name of the permission"
-     *                  ),
-     *                  @OA\Property(
-     *                      property="guard_name",
-     *                      type="string",
-     *                      description="name of the guard"
-     *                  ),
-     *                 @OA\Property(
-     *                     property="created_at",
-     *                     type="string",
-     *                     description="created date of the permission"
-     *                 ),
-     *                 @OA\Property(
-     *                     property="updated_at",
-     *                     type="string",
-     *                     description="updated date of the permission"
-     *                 )
-     *              )
-     *          )
-     *      ),
-     *       @OA\Response(
-     *           response=403, 
-     *           description="Forbidden",
-     *           @OA\JsonContent(
-     *               type="object",
-     *               @OA\Property(
-     *                   property="message",
-     *                   type="string",
-     *                   description="message"
-     *               )
-     *           )
-     *       )
-     *  )
-     */
+    
     public function index(){
         return response()->json(
             Permission::all(),
@@ -72,87 +17,7 @@ class PermissionController extends Controller
         );
     }
 
-    /**
-     *  @OA\Post(
-     *      path="/api/permission/",
-     *      tags={"Permissions"},
-     *      summary="Create a permission",
-     *      description="Create a permission",
-     *      security={{"bearer": {} }},
-     *      @OA\RequestBody(
-     *          required=true,
-     *          @OA\JsonContent(
-     *              required={"name"},
-     *              @OA\Property(property="name", type="string", example="worker")
-     *          )
-     *      ),
-     *       @OA\Response(
-     *           response=201,
-     *           description="Created",
-     *           @OA\JsonContent(
-     *               type="object",
-     *               @OA\Property(
-     *                   property="message",
-     *                   type="string",
-     *                   description="message"
-     *               ),
-     *               @OA\Property(
-     *                   property="permission",
-     *                   type="object",
-     *                  @OA\Property(
-     *                      property="id",
-     *                      type="integer",
-     *                      description="Id of the permission"
-     *                  ),
-     *                  @OA\Property(
-     *                      property="name",
-     *                      type="string",
-     *                      description="Name of the permission"
-     *                  ),
-     *                  @OA\Property(
-     *                      property="guard_name",
-     *                      type="string",
-     *                      description="name of the guard"
-     *                  ),
-     *                 @OA\Property(
-     *                     property="created_at",
-     *                     type="string",
-     *                     description="created date of the permission"
-     *                 ),
-     *                 @OA\Property(
-     *                     property="updated_at",
-     *                     type="string",
-     *                     description="updated date of the permission"
-     *                 ) 
-     *               )
-     *           )
-     *       ),
-     *       @OA\Response(
-     *           response=422,
-     *           description="Unprocessable Content",
-     *           @OA\JsonContent(
-     *               type="object",
-     *               @OA\Property(
-     *                   property="message",
-     *                   type="string",
-     *                   description="message"
-     *               )
-     *           )
-     *       ),
-     *       @OA\Response(
-     *           response=403, 
-     *           description="Forbidden",
-     *           @OA\JsonContent(
-     *               type="object",
-     *               @OA\Property(
-     *                   property="message",
-     *                   type="string",
-     *                   description="message"
-     *               )
-     *           )
-     *       )
-     *  )
-     */
+    
     public function store(Request $request){
         $validation = Validator::make(
             $request->all(),
@@ -183,69 +48,7 @@ class PermissionController extends Controller
         );
     }
 
-    /**
-     *  @OA\Get(
-     *      path="/api/permission/{id}",
-     *      tags={"Permissions"},
-     *      summary="Get a permission",
-     *      description="Get a permission",
-     *      security={{"bearer": {} }},
-     *      @OA\Parameter(
-     *          name="id",
-     *          in="path",
-     *          required=true,
-     *          @OA\Schema(type="integer")
-     *      ),
-     *      @OA\Response(
-     *          response=200,
-     *          description="Ok",
-     *          @OA\JsonContent(
-     *              type="object",
-     *              @OA\Property(
-     *                  property="permission",
-     *                  type="object",
-     *                  @OA\Property(
-     *                      property="id",
-     *                      type="integer",
-     *                      description="Id of the permission"
-     *                  ),
-     *                  @OA\Property(
-     *                      property="name",
-     *                      type="string",
-     *                      description="Name of the permission"
-     *                  ),
-     *                  @OA\Property(
-     *                      property="guard_name",
-     *                      type="string",
-     *                      description="name of the guard"
-     *                  ),
-     *                 @OA\Property(
-     *                     property="created_at",
-     *                     type="string",
-     *                     description="created date of the permission"
-     *                 ),
-     *                 @OA\Property(
-     *                     property="updated_at",
-     *                     type="string",
-     *                     description="updated date of the permission"
-     *                 )
-     *              )
-     *          )
-     *      ),
-     *       @OA\Response(
-     *           response=403, 
-     *           description="Forbidden",
-     *           @OA\JsonContent(
-     *               type="object",
-     *               @OA\Property(
-     *                   property="message",
-     *                   type="string",
-     *                   description="message"
-     *               )
-     *           )
-     *       )
-     *  )
-     */
+    
     public function show($id){
 
         $permission = Permission::find($id);
@@ -265,105 +68,7 @@ class PermissionController extends Controller
         );
     }
 
-    /**
-     *  @OA\Put(
-     *      path="/api/permission/{id}",
-     *      tags={"Permissions"},
-     *      summary="Update a permission",
-     *      description="Update a permission",
-     *      security={{"bearer": {} }},
-     *      @OA\Parameter(
-     *          name="id",
-     *          in="path",
-     *          required=true,
-     *          @OA\Schema(type="integer")
-     *      ),
-     *      @OA\RequestBody(
-     *          required=true,
-     *          @OA\JsonContent(
-     *              required={"name"},
-     *              @OA\Property(property="name", type="string", example="worker")
-     *          )
-     *      ),
-     *       @OA\Response(
-     *           response=200,
-     *           description="Ok",
-     *           @OA\JsonContent(
-     *               type="object",
-     *               @OA\Property(
-     *                   property="message",
-     *                   type="string",
-     *                   description="message"
-     *               ),
-     *               @OA\Property(
-     *                   property="permission",
-     *                   type="object",
-     *                       @OA\Property(
-     *                           property="id",
-     *                           type="integer",
-     *                           description="Id of the permission"
-     *                       ),
-     *                       @OA\Property(
-     *                           property="name",
-     *                           type="string",
-     *                           description="Name of the permission"
-     *                       ),
-     *                       @OA\Property(
-     *                           property="guard_name",
-     *                           type="string",
-     *                           description="name of the guard"
-     *                       ),
-     *                      @OA\Property(
-     *                          property="created_at",
-     *                          type="string",
-     *                          description="created date of the permission"
-     *                      ),
-     *                      @OA\Property(
-     *                          property="updated_at",
-     *                          type="string",
-     *                          description="updated date of the permission"
-     *                      )
-     *               )
-     *           )
-     *       ),
-     *       @OA\Response(
-     *           response=422,
-     *           description="Unprocessable Content",
-     *           @OA\JsonContent(
-     *               type="object",
-     *               @OA\Property(
-     *                   property="message",
-     *                   type="string",
-     *                   description="message"
-     *               )
-     *           )
-     *       ),
-     *       @OA\Response(
-     *           response=404,
-     *           description="Not found",
-     *           @OA\JsonContent(
-     *               type="object",
-     *               @OA\Property(
-     *                   property="message",
-     *                   type="string",
-     *                   description="message"
-     *               )
-     *           )
-     *       ),
-     *       @OA\Response(
-     *           response=403, 
-     *           description="Forbidden",
-     *           @OA\JsonContent(
-     *               type="object",
-     *               @OA\Property(
-     *                   property="message",
-     *                   type="string",
-     *                   description="message"
-     *               )
-     *           )
-     *       )
-     *  )
-     */
+    
     public function update(Request $request, $id){
         $validation = Validator::make(
             $request->all(),
@@ -403,57 +108,6 @@ class PermissionController extends Controller
         );
     }
 
-    /**
-     *  @OA\Delete(
-     *      path="/api/permission/{id}",
-     *      tags={"Permissions"},
-     *      summary="Delete a permission",
-     *      description="Delete a permission",
-     *      security={{"bearer": {} }},
-     *      @OA\Parameter(
-     *          name="id",
-     *          in="path",
-     *          required=true,
-     *          @OA\Schema(type="integer")
-     *      ),
-     *       @OA\Response(
-     *           response=200,
-     *           description="Ok",
-     *           @OA\JsonContent(
-     *               type="object",
-     *               @OA\Property(
-     *                   property="message",
-     *                   type="string",
-     *                   description="message"
-     *               )
-     *           )
-     *       ),
-     *       @OA\Response(
-     *           response=404,
-     *           description="Not found",
-     *           @OA\JsonContent(
-     *               type="object",
-     *               @OA\Property(
-     *                   property="message",
-     *                   type="string",
-     *                   description="message"
-     *               )
-     *           )
-     *       ),
-     *       @OA\Response(
-     *           response=403, 
-     *           description="Forbidden",
-     *           @OA\JsonContent(
-     *               type="object",
-     *               @OA\Property(
-     *                   property="message",
-     *                   type="string",
-     *                   description="message"
-     *               )
-     *           )
-     *       )
-     *  )
-     */
     public function destroy($id){
 
         $permission = Permission::find($id);
